138c394fedf9b90a225bbc9c5d70372eeb4e07be
Jon Ramer <jramer87@gmail.com>
2022-04-29 18:35:03 -0500

    Add link to problem in README and cleanup

--- sudoku_validator/README.md
+++ sudoku_validator/README.md
@@ -1,4 +1,4 @@
-### Sudoku Solution Validator
+### [Sudoku Solution Validator](https://www.codewars.com/kata/529bf0e9bdf7657179000008/train/javascript)
 Write a function validSolution/ValidateSolution/valid_solution() that accepts a 2D array representing a Sudoku board, and returns true if it is a valid solution, or false otherwise. The cells of the sudoku board may also contain 0's, which will represent empty cells. Boards containing one or more zeroes are considered to be invalid solutions.
 
 The board is always 9 cells by 9 cells, and every cell only contains integers from 0 to 9.
--- sudoku_validator/sudoku_solution_validator.js
+++ sudoku_validator/sudoku_solution_validator.js
@@ -1,10 +1,12 @@
 function validSolution(sudokuArray) {
   let validSolution = true
 
-  // Return false if you find any zeros
+  // Check for zeros
   validSolution = findZeros(sudokuArray)
+  if (!validSolution) {
+    return validSolution
+  }
 
-  // Check horizontal rows for numbers 1-9
   validSolution = validateArrayGroup(sudokuArray)
   if (!validSolution) {
     return validSolution
@@ -12,8 +14,6 @@ function validSolution(sudokuArray) {
 
   // Create arrays out of each column
   const verticalArrays = createVerticalArrays(sudokuArray)
-
-  // Check vertical 9x9 arrays for numbers 1-9
   validSolution = validateArrayGroup(verticalArrays)
   if (!validSolution) {
     return validSolution
@@ -21,22 +21,18 @@ function validSolution(sudokuArray) {
 
   // ToDo: Create 3x3 sub-arrays
   const subArrays = createSubArrays(sudokuArray)
-  // ToDo: Check 3x3 sub-arrays
   validSolution = validateArrayGroup(subArrays)
   return validSolution
 }
 
 function findZeros(twoDArray) {
   const hasZeros = twoDArray.some(e => e.includes(0)) ? false : validSolution
-  if (!hasZeros) {
-    return hasZeros
-  }
   return hasZeros
 }
 
 function validateArrayGroup(twoDArray) {
-  if (twoDArray.length < 9) {
-    throw new Error('Insufficient Array Length: ', `Array has only ${twoDArray.length} items.`)
+  if (twoDArray.length !== 9) {
+    throw new Error('Insufficient Array Length: ', `Array has ${twoDArray.length} items instead of 9.`)
   }
   for (let i = 0; i < twoDArray.length; i++) {
     const nestedArray = twoDArray[i]
@@ -45,8 +41,6 @@ function validateArrayGroup(twoDArray) {
       for (let i = 0; i < requiredNums.length; i++) {
         const element = requiredNums[i];
         if (!nestedArray.includes(element)) {
-          console.log('Nested Array: ', nestedArray, 'does not include: ', element)
-          console.log('FOUND an invalid row returning: ', false)
           return false
         }
       }
@@ -104,50 +98,3 @@ function arrayHasItems(twoDArray) {
   }
   return false
 }
-
-// validSolution([
-//   [5, 3, 4, 6, 7, 8, 9, 1, 2],
-//   [6, 7, 2, 1, 9, 5, 3, 4, 8],
-//   [1, 9, 8, 3, 4, 2, 5, 6, 7],
-//   [8, 5, 9, 7, 6, 1, 4, 2, 3],
-//   [4, 2, 6, 8, 5, 3, 7, 9, 1],
-//   [7, 1, 3, 9, 2, 4, 8, 5, 6],
-//   [9, 6, 1, 5, 3, 7, 2, 8, 4],
-//   [2, 8, 7, 4, 1, 9, 6, 3, 5],
-//   [3, 4, 5, 2, 8, 6, 1, 7, 9]
-// ]); // => true
-// validSolution([
-//   [5, 3, 4, 6, 7, 8, 9, 1, 2],
-//   [6, 7, 2, 1, 9, 0, 3, 4, 8],
-//   [1, 0, 0, 3, 4, 2, 5, 6, 0],
-//   [8, 5, 9, 7, 6, 1, 0, 2, 0],
-//   [4, 2, 6, 8, 5, 3, 7, 9, 1],
-//   [7, 1, 3, 9, 2, 4, 8, 5, 6],
-//   [9, 0, 1, 5, 3, 7, 2, 1, 4],
-//   [2, 8, 7, 4, 1, 9, 6, 3, 5],
-//   [3, 0, 0, 4, 8, 1, 1, 7, 9]
-// ]); // => false
-
-// validSolution([
-//   [1, 2, 6, 3, 4, 7, 5, 9, 8],
-//   [7, 3, 5, 8, 1, 9, 6, 4, 2],
-//   [1, 9, 4, 2, 7, 5, 8, 6, 3],
-//   [3, 1, 7, 5, 8, 4, 2, 6, 9],
-//   [7, 5, 9, 1, 6, 2, 4, 3, 8],
-//   [4, 8, 2, 9, 3, 6, 7, 1, 5],
-//   [1, 4, 8, 7, 5, 9, 3, 2, 6],
-//   [5, 6, 1, 4, 2, 3, 9, 8, 7],
-//   [2, 7, 3, 6, 9, 1, 8, 5, 4]
-// ]); // => false
-
-// validSolution([
-//   [1,2,3,4,5,6,7,8,9],
-//   [2,3,1,5,6,4,8,9,7],
-//   [3,1,2,6,4,5,9,7,8],
-//   [4,5,6,7,8,9,1,2,3],
-//   [5,6,4,8,9,7,2,3,1],
-//   [6,4,5,9,7,8,3,1,2],
-//   [7,8,9,1,2,3,4,5,6],
-//   [8,9,7,2,3,1,5,6,4],
-//   [9,7,8,3,1,2,6,4,5]
-// ]) // => false
